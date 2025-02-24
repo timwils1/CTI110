549ddeb4dae052ce516f56bca1908509bbd5f5e1
timwils1 <wilsont0546@student.faytechcc.edu>
2025-02-24 00:38:25 -0500

    Add files via upload

---
+++ P1LAB1_WilsonTimothy.py
@@ -0,0 +1,8 @@
+# Timothy Wilson
+# February 23, 2025
+# P1LAB1
+# The lab is a continuation of the reading in ZYBOOKS
+
+first_name = input("Enter your first name: Timothy")
+last_name = input("Enter your last name: Wilson")
+print("\nHello,", Timothy, Wilson + "! Welcome to CTI-110")
