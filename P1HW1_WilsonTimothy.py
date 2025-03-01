671b409a30d62e376ea1307d8e48a89e8d5e6769
timwils1 <wilsont0546@student.faytechcc.edu>
2025-03-01 03:31:15 -0500

    Add files via upload

---
+++ P1HW1_WilsonTimothy.py
@@ -0,0 +1,11 @@
+# Timothy Wilson
+# March 1, 2025
+# P1HW1_WilsonTimothy
+# Calculating exponents and addition and subtraction
+
+
+
+print(f"\n{7} raised to the power of {3} is {343}!!!\n")
+
+print(f"{10} + {6} - {4} is equal to {12}")
+
