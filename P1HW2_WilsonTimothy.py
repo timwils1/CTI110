429eea3499212d93b58333d2c1f01df7afd32bca
timwils1 <wilsont0546@student.faytechcc.edu>
2025-03-01 23:39:39 -0500

    Add files via upload

---
+++ P1HW2_WilsonTimothy.py
@@ -0,0 +1,47 @@
+# Timothy Wilson
+# March 1, 2025
+# P1HW2_WilsonTimothy
+# Balancing the Budget
+
+float(input("Enter your budget: $"))
+print(f"Initial Budget: $1200.00\n")
+print("Location: Nashville")
+print("Fuel: $250.00")
+print("Accommodation: $600.00")
+print("Food: $300.00")
+
+print(f"Fuel: ${250:.0f}")
+print(f"Accommodation: ${600:.0f}")
+print(f"Food: ${300:.0f}")
+
+print(f"Initial Budget: ${budget:.2f}\n")
+print(f"Fuel: ${fuel_expense:.2f}")
+print(f"Accommodation: ${accommodation_expense:.2f}")
+print(f"Food: ${food_expense:.2f}")
+print(f"Total Expenses: ${total_expenses:.2f}")
+print(f"Remaining Balance: ${remaining_balance:.2f}")
+
+
+print(f"Initial Budget: ${1200:.0f}\n")
+print("Fuel: $250.00")
+print(f"Accommodation: ${600:.0f}")
+print(f"Food: ${300:.0f}")
+print(f"Total Expenses: ${1150:.0f}")  
+print(f"Remaining Balance: ${50:.0f}")
+
+input("Enter your budget (default $1200.00): $")
+print(f"Initial Budget: ${1200:.0f}\n")
+print("Fuel: $250.00")
+print(f"Accommodation: ${600:.0f}")
+print(f"Food: ${300:.0f}")
+print(f"Total Expenses: ${1150:.0f}")  
+print(f"Remaining Balance: ${50:.0f}")
+
+
+
+
+
+
+
+
+
