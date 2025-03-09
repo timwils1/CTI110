e1478301c8676ef5a75fd9345947d39768af1d58
timwils1 <wilsont0546@student.faytechcc.edu>
2025-03-09 01:16:48 -0500

    Add files via upload

---
+++ P2LAB1_WilsonTimothy.py
@@ -0,0 +1,17 @@
+#Timothy Wilson
+#March 9, 2025
+#P2LAB1
+#Variables and Expressions
+
+import math
+
+radius = 8.75
+
+diameter = 2 * radius
+circumference = 2 * math.pi * radius
+area = math.pi * (radius ** 2)
+
+print(f"What is the radius of the circle? {radius}")
+print(f"The diameter of the circle is {diameter}")
+print(f"The circumference of the circle is {circumference:.2f}")
+print(f"The area of the circle is {area:.3f}")
