d43abac3386665f4851de57fa7e5db164596ac38
timwils1 <wilsont0546@student.faytechcc.edu>
2025-03-09 04:24:26 -0400

    Add files via upload

---
+++ P1HW1_WilsonTim.py
@@ -0,0 +1,32 @@
+#Timothy Wilson
+#March 2, 2025
+#P1HW1
+#Integers
+
+
+
+
+print("-----Calculating Exponents----\n")
+
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+
+result = base ** exponent
+
+
+print(f"\n{base} raised to the power of {exponent} is {result} !!\n")
+
+# print("-----Addition and Subtraction----\n")
+
+
+start_value = int(input("Enter a starting integer: "))
+add_value = int(input("Enter an integer to add: "))
+subtract_value = int(input("Enter an integer to subtract: "))
+
+
+final_result = start_value + add_value - subtract_value
+
+
+print(f"\n{start_value} + {add_value} - {subtract_value} is equal to {final_result}")
